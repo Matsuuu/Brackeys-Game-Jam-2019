d4e8443a84b067d06f70c6dcf49392fd6d3e3685
Matsuuu <huhta.matias@gmail.com>
2019-02-21 21:14:53 +0200

    Endless scrolling background

---
+++ Assets/Camera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    public bool scroll;
+
+    public float cameraMoveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (scroll)
+        {
+            ScrollCamera();
+        }
+    }
+
+    void ScrollCamera()
+    {
+        transform.Translate(Vector3.right * cameraMoveSpeed * 0.1f);
+    }
+}
