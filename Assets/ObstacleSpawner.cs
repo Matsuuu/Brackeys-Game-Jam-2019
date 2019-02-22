93ca9dce307ae53df943c7fb6553ef808fdf0033
Matsuuu <huhta.matias@gmail.com>
2019-02-22 17:06:27 +0200

    Ground follows background

---
+++ Assets/ObstacleSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    public GameObject[] obstacles;
+
+    private GameObject spawner;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawner = GameObject.FindGameObjectWithTag("Spawner");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnObstacle()
+    {
+        
+    }
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] obstacles;
+    public Transform obstacleHolder;
+    public Vector3 offset;
+
+    private Transform camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GameObject.FindGameObjectWithTag("MainCamera").transform;
+        obstacleHolder = GameObject.FindGameObjectWithTag("ObstacleHolder").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            SpawnObstacle();
+        }
+        Vector3 pos = transform.position;
+        transform.position = new Vector3(camera.position.x + offset.x, pos.y, pos.z);
+    }
+
+    public void SpawnObstacle()
+    {
+        int obstaclenum = Random.Range(0, obstacles.Length);
+        GameObject obstacleToSpawn = obstacles[obstaclenum];
+        GameObject obs = Instantiate(obstacleToSpawn, transform.position, transform.rotation);
+        obs.transform.parent = obstacleHolder;
+    }
+}
